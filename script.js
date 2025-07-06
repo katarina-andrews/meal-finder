9015f0c69c71c961a7a3f7f5af564b1bcd45dec0
katarina-andrews <andrews.katarina@gmail.com>
2025-07-06 15:05:51 -0500

    fixed the api link. changed some variable names

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const randomBtn = document.getElementById("random-btn");
 
 async function fetchRandom() {
   try {
-    const response = await fetch("www.themealdb.com/api/json/v1/1/random.php");
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     if (!response.ok) {
       throw new Error("Network error. Status: ", response.status);
     }
@@ -19,65 +19,73 @@ async function fetchRandom() {
   }
 }
 
-function renderRandomRecipe(recipeData) {
+// data.meals 
+
+function renderRandomMeal(mealData) {
   recipeContainer.innerHTML = "";
 
-  recipeData.forEach((recipe) => {
-    const recipeElm = document.createElement("div");
-    recipeElm.className = "";
+  mealData.forEach((meal) => {
+    const mealElm = document.createElement("div");
+    mealElm.className = "";
 
     const nameElm = document.createElement("h1");
-    nameElm.innerHTML = recipe.strMeal;
+    nameElm.innerHTML = meal.strMeal;
     nameElm.className = "";
 
     const categoryElm = document.createElement("p");
-    categoryElm.innerHTML = `Category: ${recipe.strCategory}`;
+    categoryElm.innerHTML = `Category: ${meal.strCategory}`;
     categoryElm.className = "";
 
     const areaElm = document.createElement("p");
-    areaElm.innerHTML = `Origin: ${recipe.strArea}`;
+    areaElm.innerHTML = `Origin: ${meal.strArea}`;
     areaElm.className = "";
 
     const instructionElm = document.createElement("p");
-    instructionElm.innerHTML = `Instructions: ${recipe.strInstructions}`;
+    instructionElm.innerHTML = `Instructions: ${meal.strInstructions}`;
     instructionElm.className = "";
 
     const imgElm = document.createElement("img");
-    imgElm.src = recipe.strMealThumb;
-    imgElm.alt = recipe.strMeal;
+    imgElm.src = meal.strMealThumb;
+    imgElm.alt = meal.strMeal;
     imgElm.className = "";
 
     const tagsElm = document.createElement("p");
-    tagsElm.innerHTML = `Tags: ${recipe.strTags}`;
+    tagsElm.innerHTML = `Tags: ${meal.strTags}`;
     tagsElm.className = "";
 
     const videoLinkElm = document.createElement("a");
-    videoLinkElm.href = recipe.strYoutube;
-    videoLinkElm.innerText = "Recipe Video";
+    videoLinkElm.href = meal.strYoutube;
+    videoLinkElm.innerHTML = "Recipe Video";
     videoLinkElm.target = "_blank";
     videoLinkElm.className = "";
 
-    // change these to for loops 1 - 20 for both 
-    // const ingredientElm = document.createElement("p");
-    // ingredientElm.innerHTML = recipe.strIngredient;
-    // ingredientElm.className = "";
 
-    // const measureElm = document.createElement("p");
-    // measureElm.innerHTML = recipe.strMeasure;
-    // measureElm.className = "";
 
-    recipeElm.appendChild(nameElm);
-    recipeElm.appendChild(imgElm);
-    // recipeElm.appendChild(ingredientElm);
-    // recipeElm.appendChild(measureElm);
-    recipeElm.appendChild(instructionElm);
-    recipeElm.appendChild(categoryElm);
-    recipeElm.appendChild(areaElm);
-    recipeElm.appendChild(tagsElm);
-    recipeElm.appendChild(videoLinkElm);
+    // // change these to for loops 1 - 20 for both 
+    //  const ingrAndMeasListElm = document.createElement("ul");
+    //  for (let i = 1; i <= 20; i++) {
 
-    recipeContainer.appendChild(recipeElm);
+    //  }
+    // // ingredientElm.innerHTML = meal.strIngredient;
+    // // ingrAndMeasListElm.className = "";
+
+    // // const measureElm = document.createElement("p");
+    // meal.strMeasure;
+  
+
+    mealElm.appendChild(nameElm);
+    mealElm.appendChild(imgElm);
+    // mealElm.appendChild(ingrAndMeasListElm);
+    mealElm.appendChild(instructionElm);
+    mealElm.appendChild(categoryElm);
+    mealElm.appendChild(areaElm);
+    mealElm.appendChild(tagsElm);
+    mealElm.appendChild(videoLinkElm);
+
+    recipeContainer.appendChild(mealElm);
   });
 }
 
-randomBtn.addEventListener("click", renderRandomRecipe);
+randomBtn.addEventListener("click", renderRandomMeal);
+
+
